3355114183d73f3513e4adb8f8720fbdfb6d8ca1
Travis Frels <tfrels@squareup.com>
2022-02-11 10:53:58 -0600

    refactor components

--- src/components/HelloFlex/HelloFlex.jsx
+++ src/components/HelloFlex/HelloFlex.jsx
@@ -2,38 +2,56 @@ import React from "react";
 import { HelloFlexComponentStyles } from "./HelloFlex.Styles";
 import { Actions, TaskHelper } from "@twilio/flex-ui";
 
-const HelloFlex = (props) => {
-  const chanSid = TaskHelper.getTaskChatChannelSid(props.task);
-  const macros = ["macro 1", "macro 2"];
+const MacroButton = ({ action, message, title, sid }) => (
+  <button
+    onClick={() => {
+      Actions.invokeAction(action, {
+        body: message,
+        channelSid: sid,
+      });
+    }}
+  >
+    {title}
+  </button>
+);
+
+const Macro = ({ macro: { id, text }, sid }) => (
+  <li key={id}>
+    <label>
+      {text}&nbsp;&nbsp;
+      <MacroButton action="SendMessage" message={text} title="send" sid={sid} />
+      <MacroButton
+        action="SetInputText"
+        message={text}
+        title="type"
+        sid={sid}
+      />
+    </label>
+  </li>
+);
+
+const HelloFlex = ({ task }) => {
+  let chanSid = "";
+  try {
+    chanSid = TaskHelper.getTaskChatChannelSid(task);
+  } catch (err) {
+    return (
+      <HelloFlexComponentStyles>
+        <h1>I'm not in a conversation.</h1>
+      </HelloFlexComponentStyles>
+    );
+  }
+
+  const macros = [
+    { id: 1, text: "macro 1" },
+    { id: 2, text: "macro 2" },
+  ];
+
   return (
     <HelloFlexComponentStyles>
       <ul>
         {macros.map((m) => (
-          <li>
-            <label>
-              {m}&nbsp;&nbsp;
-              <button
-                onClick={() => {
-                  Actions.invokeAction("SendMessage", {
-                    body: m,
-                    channelSid: chanSid,
-                  });
-                }}
-              >
-                message
-              </button>
-              <button
-                onClick={() => {
-                  Actions.invokeAction("SetInputText", {
-                    body: m,
-                    channelSid: chanSid,
-                  });
-                }}
-              >
-                input
-              </button>
-            </label>
-          </li>
+          <Macro macro={m} sid={chanSid} />
         ))}
       </ul>
     </HelloFlexComponentStyles>
